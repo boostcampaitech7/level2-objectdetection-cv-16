2e8d1198c0c8a0459bb53944510c8badbeaaa412
kkh090 <kihoon090@gmail.com>
2024-09-30 18:56:46 +0900

    base/ files and folder added

---
+++ model/model_selection.py
@@ -0,0 +1,33 @@
+import timm
+import torch
+import torch.nn as nn
+from model.cnn import SimpleCNN
+from model.mlp import MLP
+from model.torchvision_model import TorchvisionModel
+from model.timm import TimmModel
+from model.resnet18 import ResNetModel
+
+class ModelSelector:
+    """
+    사용할 모델 유형을 선택하는 클래스.
+    """
+    def __init__(
+        self, 
+        model_type: str, 
+        num_classes: int, 
+        **kwargs
+    ):
+        # 모델 유형을 소문자로 변환
+        model_type = model_type.lower()
+        
+        if model_type == '':
+            pass
+        elif model_type == '':
+            pass
+        else:
+            raise ValueError("Unknown model type specified.")
+
+    def get_model(self) -> nn.Module:
+
+        # 생성된 모델 객체 반환
+        return self.model
