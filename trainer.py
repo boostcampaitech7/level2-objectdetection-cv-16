929b879d86dab43d4d941b8c18ca2e242c16a4ac
kkh090 <kihoon090@gmail.com>
2024-10-09 03:36:15 +0900

    base/ mmdetection implemented on trainer.py

--- trainer.py
+++ trainer.py
@@ -74,21 +74,18 @@ class Trainer: # 변수 넣으면 바로 학습되도록
         train_loss = 0.0
         progress_bar = tqdm(train_loader, desc="Training", leave=False, disable=self.verbose)
         
-        for images, targets, image_ids in progress_bar:
-            images = list(image.float().to(self.device) for image in images)
-            targets = [{k: v.to(self.device) for k, v in t.items()} for t in targets]
-            
+        for data in progress_bar:
             self.optimizer.zero_grad()
             
-            loss_dict = self.model(images, targets)
+            loss_dict = self.model.train_step(data, self.optimizer)
             
-            losses = sum(loss for loss in loss_dict.values())
+            losses = loss_dict['loss']
             loss_value = losses.item()
             
             losses.backward()
             self.optimizer.step()
             
-            train_loss += loss_value * len(images)
+            train_loss += loss_value * data['img'].data[0].shape[0]
             progress_bar.set_postfix(loss=loss_value)
         
         train_loss = train_loss / train_loader.dataset.__len__()
@@ -105,23 +102,19 @@ class Trainer: # 변수 넣으면 바로 학습되도록
         log_images= [] #wandb 로그에 올릴 이미지 저장
         
         with torch.no_grad():
-            for images, targets, image_ids in progress_bar:
-                images = list(image.float().to(self.device) for image in images)
-                targets = [{k: v.to(self.device) for k, v in t.items()} for t in targets]
+            for data in progress_bar:
+                data = [{k: data[k][i] for k in data} for i in range(len(next(iter(data.values()))))]
                 
-                ## loss값을 뽑아내도록 해놓긴 했으나, eval모드에서 bbox 예측값을 가지고 처리할 수 있도록
-                ## 작성 해야함 - 추후 수정 예정
-                self.model.train()
-                loss_dict = self.model(images, targets)
-                self.model.eval()
-                
-                losses = sum(loss for loss in loss_dict.values())
-                loss_value = losses.item()
-                
-                val_loss += loss_value * len(images)
-                progress_bar.set_postfix(loss=loss_value)
+                for _data in data:
+                    loss_dict = self.model.val_step(_data, self.optimizer)
+                    
+                    losses = loss_dict['loss']
+                    loss_value = losses.item()
+                    
+                    val_loss += loss_value * _data['img'].data[0].shape[0]
+                    progress_bar.set_postfix(loss=loss_value)
 
-        val_loss = val_loss / val_loader.dataset.__len__()
+        val_loss = val_loss / (val_loader.dataset.__len__() * len(data))
         return val_loss
 
     def train(self) -> None:
@@ -222,88 +215,137 @@ class Trainer: # 변수 넣으면 바로 학습되도록
 def collate_fn(batch):
     return tuple(zip(*batch))
 
-## 테스트용
-# if __name__=='__main__':
-#     ## device와 seed 설정
-#     from util.augmentation import TransformSelector
-#     from util.data import CustomDataset, get_kfold_json
-#     import torchvision
-#     from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
+# 테스트용
+if __name__=='__main__':
+    ## device와 seed 설정
+    from util.augmentation import TransformSelector
+    from util.data import CustomDataset, get_kfold_json
+    import torchvision
+    from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
 
-#     from util.optimizers import get_optimizer
-#     from util.schedulers import get_scheduler
-#     from config.custom_json_parser import Custom_json_parser
+    from util.optimizers import get_optimizer
+    from util.schedulers import get_scheduler
+    from config.custom_json_parser import Custom_json_parser
     
-#     config_json_path = "config/train.json"
+    config_json_path = "config/train_config.json"
 
-#     config_parser = Custom_json_parser(mode="train", config_json_path=config_json_path)
-#     config = config_parser.get_config_from_json()
+    config_parser = Custom_json_parser(mode="train", config_json_path=config_json_path)
+    config = config_parser.get_config_from_json()
     
-#     device = torch.device(config['device'])
+    device = torch.device(config['device'])
     
-#     kfold_annotations = get_kfold_json(random_seed=config['random_seed'], **config['kfold'])
+    kfold_annotations = get_kfold_json(random_seed=config['random_seed'], **config['kfold'])
     
-#     img_size = config['data']['img_size']
+    img_size = config['data']['img_size']
     
-#     ## 데이터 증강 및 세팅
-#     transform_selector = TransformSelector(transform_type=config['augmentation']['name'],
-#                                            common_transform=config['augmentation']['common_transform'])
+    from mmdet.datasets import build_dataset, build_dataloader
+    from mmcv import Config
+    from mmdet.utils import get_device, build_dp
     
-#     train_transform = transform_selector.get_transform(augment=True,
-#                                                        kwargs=config['augmentation']['train_transform'])
-#     val_transform = transform_selector.get_transform(augment=False, 
-#                                                      kwargs=config['augmentation']['val_transform'])
+    classes = ("General trash", "Paper", "Paper pack", "Metal", "Glass", 
+           "Plastic", "Styrofoam", "Plastic bag", "Battery", "Clothing")
 
-#     train_dataset = CustomDataset(config['data']['train_json'], config['data']['data_root'], transforms=train_transform)
-#     val_dataset = CustomDataset(config['data']['val_json'], config['data']['data_root'], transforms=val_transform)
+    ## mmdetection implementation
+    cfg = Config.fromfile('./configs/faster_rcnn/faster_rcnn_r50_fpn_1x_coco.py')
     
-#     train_dataloader = DataLoader(
-#         train_dataset,
-#         batch_size=config['data']['batch_size'],
-#         shuffle=False,
-#         num_workers=0,
-#         collate_fn=collate_fn
-#     )
-    
-#     val_dataloader = DataLoader(
-#         val_dataset,
-#         batch_size=config['data']['batch_size'],
-#         shuffle=False,
-#         num_workers=0,
-#         collate_fn=collate_fn
-#     )
-    
-#     ## 학습 모델
-    
-#     # model = model_selector.get_model()
-#     model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=config['model']['pretrained'])
-#     in_features = model.roi_heads.box_predictor.cls_score.in_features
-#     model.roi_heads.box_predictor = FastRCNNPredictor(in_features, config['data']['num_classes']+1)
-#     model.to(device)
-    
-#     ## Optimizer 
-#     optimizer = get_optimizer(config['optimizer']['name'], model, config['optimizer']['kwargs'])
+    root = "./dataset/"
 
-#     ## Loss
-#     loss = None
-    
-#     ## Scheduler
-#     scheduler = get_scheduler(config['scheduler']['name'], optimizer, config['scheduler']['kwargs'])
-    
-#     model.to(device)    
+    cfg.data.train.classes = classes
+    cfg.data.train.img_prefix = root
+    cfg.data.train.ann_file = root + '10-fold_json/train_fold_1.json' # train json 정보
+    cfg.data.train.pipeline[2]['img_scale'] = (1024,1024) # Resize
 
-#     ## 학습 시작
-#     trainer = Trainer(
-#         model=model,
-#         device=device,
-#         train_loader=train_dataloader,
-#         val_loader=val_dataloader,
-#         optimizer=optimizer,
-#         scheduler=scheduler,
-#         loss_fn=loss,
-#         config_json_path=config_json_path,
-#         **config['trainer']
-#     )
+    cfg.data.val.classes = classes
+    cfg.data.val.img_prefix = root
+    cfg.data.val.ann_file = root + '10-fold_json/val_fold_1.json'
+    cfg.data.val.pipeline[2]['img_scale'] = (1024, 1024)
 
-#     trainer.train()
+    cfg.data.test.classes = classes
+    cfg.data.test.img_prefix = root
+    cfg.data.test.ann_file = root + 'test.json' # test json 정보
+    cfg.data.test.pipeline[1]['img_scale'] = (1024,1024) # Resize
+
+    cfg.data.samples_per_gpu = 8
+
+    cfg.seed = 2024
+    cfg.gpu_ids = [0]
+    cfg.work_dir = 'asd'
+
+    cfg.model.roi_head.bbox_head.num_classes = 10
+
+    cfg.optimizer_config.grad_clip = dict(max_norm=35, norm_type=2)
+    cfg.checkpoint_config = dict(max_keep_ckpts=3, interval=1)
+    cfg.device = get_device()
+    
+    train_datasets = build_dataset(cfg.data.train)
+    val_datasets = build_dataset(cfg.data.val) #, dict(test_mode=False))
+    
+    runner_type = 'EpochBasedRunner' if 'runner' not in cfg else cfg.runner['type']
+
+    train_dataloader_default_args = dict(
+        samples_per_gpu=8,
+        workers_per_gpu=2,
+        # `num_gpus` will be ignored if distributed
+        num_gpus=len(cfg.gpu_ids),
+        dist=False,
+        seed=cfg.seed,
+        runner_type=runner_type,
+        persistent_workers=False)
+
+    train_loader_cfg = {
+        **train_dataloader_default_args,
+        **cfg.data.get('train_dataloader', {})
+    }
+    
+    val_dataloader_default_args = dict(
+        samples_per_gpu=4,
+        workers_per_gpu=2,
+        num_gpus=len(cfg.gpu_ids),
+        dist=False,
+        shuffle=False,
+        runner_type=runner_type,
+        persistent_workers=False)
+
+    val_dataloader_args = {
+        **val_dataloader_default_args,
+        **cfg.data.get('val_dataloader', {})
+    }
+    
+    val_dataloader = build_dataloader(val_datasets, **val_dataloader_args)
+    train_dataloader = build_dataloader(train_datasets, **train_loader_cfg)
+    
+
+    from mmdet.models import build_detector
+    
+    model = build_detector(cfg.model)
+    model.init_weights()
+    
+    model = build_dp(model, cfg.device, device_ids=cfg.gpu_ids)
+    
+    ## end mmdetection implementation
+    ## Optimizer 
+    optimizer = get_optimizer(config['optimizer']['name'], model, config['optimizer']['kwargs'])
+
+    ## Loss
+    loss = None
+    
+    ## Scheduler
+    scheduler = get_scheduler(config['scheduler']['name'], optimizer, config['scheduler']['kwargs'])
+    
+    model.to(device)    
+
+    ## 학습 시작
+    trainer = Trainer(
+        model=model,
+        device=device,
+        train_loader=train_dataloader,
+        val_loader=val_dataloader,
+        optimizer=optimizer,
+        scheduler=scheduler,
+        loss_fn=loss,
+        config_json_path=config_json_path,
+        **config['trainer']
+    )
+
+    trainer.train()
     
