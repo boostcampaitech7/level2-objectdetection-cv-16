f8b78e5819c89a113c49a25bf4478cf58d3a5e67
kkh090 <kihoon090@gmail.com>
2024-10-14 13:17:52 +0900

    fix/ mmdetection_code folder named changed to our_code

--- our_code/exp_configs.py
+++ our_code/exp_configs.py
@@ -103,6 +103,11 @@ model = dict(
 ## Pipeline
 dataset_type = 'CocoDataset'
 data_root ='/data/ephemeral/home/kwak/level2-objectdetection-cv-16/dataset'
+ann_root = data_root + '/stratified_10fold_dataset'
+train_json = ann_root + '/train_fold_1.json'
+val_json = ann_root + '/val_fold_1.json'
+test_json = data_root + '/test.json'
+
 classes = ("General trash", "Paper", "Paper pack", "Metal", "Glass",
             "Plastic", "Styrofoam", "Plastic bag", "Battery", "Clothing")
 backend_args = None
@@ -154,7 +159,7 @@ train_dataloader = dict(
     dataset=dict(
         type=dataset_type,
         data_root=data_root,
-        ann_file='10-fold_json/train_fold_1.json',
+        ann_file= train_json,
         data_prefix=dict(img=''),
         filter_cfg=dict(filter_empty_gt=True),
         metainfo=dict(classes=classes),
@@ -172,7 +177,7 @@ val_dataloader = dict(
     dataset=dict(
         type=dataset_type,
         data_root=data_root,
-        ann_file='10-fold_json/val_fold_1.json',
+        ann_file=val_json,
         data_prefix=dict(img=''),
         test_mode=True,
         metainfo=dict(classes=classes),
@@ -189,7 +194,7 @@ test_dataloader = dict(
     dataset=dict(
         type=dataset_type,
         data_root=data_root,
-        ann_file=data_root +'test.json',
+        ann_file=test_json,
         data_prefix=dict(img=''),
         test_mode=True,
         metainfo=dict(classes=classes),
@@ -200,12 +205,12 @@ test_dataloader = dict(
 ## evaluator
 val_evaluator = dict(
     type='CocoMetric',
-    ann_file=data_root + '/10-fold_json/val_fold_1.json',
+    ann_file=val_json,
     metric=['bbox'],
     format_only=False,
     backend_args=backend_args)
 test_evaluator = dict(
-    ann_file=data_root + '/10-fold_json/val_fold_1.json')
+    ann_file=test_json)
 
 ## schedule & optimizer
 optim_wrapper = dict(
