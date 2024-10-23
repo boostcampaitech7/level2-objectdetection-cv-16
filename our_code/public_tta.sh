8a1bbc3f63398f7e559f27c3544ca82ef71b8cd9
hobbang <hocheol0303@gmail.com>
2024-10-23 14:08:49 +0900

    Real TTA

---
+++ our_code/public_tta.sh
@@ -0,0 +1,3 @@
+python ./our_code/public_tta.py /data/ephemeral/home/hobbang/level2-objectdetection-cv-16/work_dirs/configs/sota.py /data/ephemeral/home/hobbang/level2-objectdetection-cv-16/work_dirs/sota/epoch_15.pth --tta
+
+# 2번째 config, 3번째 checkpoint
